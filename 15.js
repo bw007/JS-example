4e2e0d3e4314294374bd2ad750ef996ac8daab17
bw007 <abdulaziz00791@gmail.com>
2025-03-24 07:25:27 +0500

    solved

--- 15.js
+++ 15.js
@@ -1,3 +1,4 @@
+// 40% AI yordami bilan yechilgan
 function findSecondMax(arr = []) {
   let max1 = -Infinity;
   let max2 = -Infinity;
@@ -16,3 +17,19 @@ function findSecondMax(arr = []) {
 }
 
 console.log(findSecondMax([1, 1]));
+
+
+
+// Men bilgan noprofessional yechim
+function sortNumber(arr = []) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        [ arr[i], arr[j] ] = [ arr[j], arr[i] ]
+      }
+    }
+  }
+  return arr[arr.length - 2]
+}
+
+console.log(sortNumber([2, 1, 3, 1, 0, 5]));
